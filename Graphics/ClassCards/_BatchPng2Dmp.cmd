c728dca41d608176f0e556373eaaae05d22c0f5f
Kyrads <nicholas7xyz@gmail.com>
2023-05-10 18:47:49 +0100

    Testing Class Card Installer

---
+++ Graphics/ClassCards/_BatchPng2Dmp.cmd
@@ -0,0 +1,21 @@
+@echo off
+
+@set "Png2Dmp=%~dp0/../../EventAssembler/Tools/Png2Dmp.exe"
+
+@cd %~dp0/PNG
+
+@dir *.png /b > png.txt
+
+@for /f "tokens=*" %%m in (png.txt) do ("%Png2Dmp%" "%%m" --lz77 -po "%%m_pal.dmp")
+
+@del png.txt
+
+@cd %~dp0
+
+@copy "%~dp0PNG\*.dmp" "%~dp0DMP" > nul
+
+@del "%~dp0PNG\*.dmp"
+
+echo Done!
+
+pause
