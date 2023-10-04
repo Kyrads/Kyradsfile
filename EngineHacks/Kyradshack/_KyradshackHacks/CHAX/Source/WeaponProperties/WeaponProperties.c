4324c4d8b1a50918c267c6d29ffa81e2e0b9aaee
Kyrads <nicholas7xyz@gmail.com>
2023-10-04 18:49:17 +0100

    Bunch of Items + WIP Weapon Properties

---
+++ EngineHacks/Kyradshack/_KyradshackHacks/CHAX/Source/WeaponProperties/WeaponProperties.c
@@ -0,0 +1,25 @@
+extern u8 ThrowablesList[];
+enum 
+{ 
+CannotDouble = 0, 
+ForceDouble = 1, 
+NoChange = 2,
+}; 
+
+int ThrowablesProperty(struct BattleUnit* battleUnit){
+	int i = 0;
+	while(true){
+		u8 item = ThrowablesList[i];
+		if(ITEM_INDEX(battleUnit->weapon) == item){
+			if(gBattleStats.range!=1){
+				return CannotDouble;
+			}
+			return NoChange;
+		}
+		if(item == 0){
+			break;
+		}
+		i++;
+	}
+	return NoChange;
+}
